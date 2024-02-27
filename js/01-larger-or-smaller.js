db1e6300d7cbe7871e7b8965d8c7fb6800a89daa
FauziaShafi <fauziashafi12@gmail.com>
2024-02-27 11:36:11 -0800

    initial commit

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,17 @@
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
+
+if(num1>num2) {
+
+    window.alert("First number is greater");
+
+} else if(num2>num1) {
+
+    window.alert("Second number is greater");
+
+}
+ else {
+    
+    window.alert("They are equal");
+
+}
